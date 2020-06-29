72ca9e8252a10eba6ef77648c4dd523adb561cf6
Tomiwa Obanla <obanlatomiwa@gmail.com>
2020-06-29 02:14:08 +0100

    modified babel to lastest versions

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,6 @@
 // Global app controller
 
 import num from './test';
+const x = 23;
 
-console.log(`I imported ${num} from a module test.js`);
+console.log(`I imported ${num} from a module test.js! Variable x is ${x}`);
